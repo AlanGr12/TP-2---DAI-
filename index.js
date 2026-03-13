51c83955b50a62ac9588dc6a399ce22383845a98
ALAN GABRIEL GORDON <48860730@ort>
2026-03-13 12:09:47 -0300

    avanzando el tp

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hola")
+//Ejercicio 1 
+
+const stringUno = console.log('Hola');
+const stringDos = console.log('Todo Bien?'); 
+
+console.log('' + stringUno + '' + stringDos);
+console.log(`${stringUno} ${stringDos}`);
+
+//Ejercicio 2
+
