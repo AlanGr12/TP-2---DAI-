320fe65b1fb26082d32fa2cbd8651052657ca9c0
AlanGr12 <alanfutbol58@gmail.com>
2026-03-15 16:11:53 -0300

    ej2 js

---
+++ matematica.js
@@ -0,0 +1,21 @@
+const PI = 3.14
+
+function sumar(num1 , num2){
+ return num1 + num2
+
+}
+
+let restar = (num1 , num2) => {
+return num1 - num2
+}
+
+
+function multiplicar(num1 , num2){
+ return num1 * num2
+
+}
+
+
+let dividir = (num1 , num2) => {
+return num1 / num2
+}
