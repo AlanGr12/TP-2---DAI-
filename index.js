41970e4282189f1914322720c8a369c77ffbae37
AlanGr12 <alanfutbol58@gmail.com>
2026-03-15 16:23:57 -0300

    ej 2 terminado

--- index.js
+++ index.js
@@ -7,7 +7,19 @@ console.log('' + stringUno + '' + stringDos);
 console.log(`${stringUno} ${stringDos}`);
 
 //Ejercicio 2
-// hacer esto antes y despues cambiar matematica mkdir src mkdir src/modules
+// hacer esto antes y despues cambiar matematica mkdir src mkdir src/modules y hacer el import import {sumar, restar, multiplicar, dividir, PI} from './src/modules/matematica.js'
 
+const resultadoUno = sumar(10, 2)
+console.log(resultadoUno)
 
+const resultadoDos = restar(10, 2)
+console.log(resultadoDos)
+
+const resultadoTres = multiplicar(10, 2)
+console.log(resultadoTres)
+
+const resultadoCuatro = dividir(10, 2)
+console.log(resultadoCuatro)
+
+console.log(`Numero ${PI}`)
 
