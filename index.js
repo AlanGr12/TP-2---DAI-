7d7c2269ca8ccb1702ed0a4934555e67e7cdec96
AlanGr12 <alanfutbol58@gmail.com>
2026-03-15 18:31:23 -0300

    ej 3 terminado

--- index.js
+++ index.js
@@ -24,3 +24,10 @@ console.log(resultadoCuatro)
 console.log(`Numero ${PI}`)
 //Ejercicio 3
 // crear la carpeta models con mkdir src/models y dsp la carpeta alumno.js
+import Alumno from './src/models/alumno.js'
+
+const nuevoAlumno = new Alumno("Juan",12345678)
+const nuevoAlumnoDos = new Alumno("Mati",21356878)
+
+console.log(`El dni del ${nuevoAlumno.username} es ${nuevoAlumno.DNI}`)
+console.log(`El dni del ${nuevoAlumnoDos.username} es ${nuevoAlumnoDos.DNI}`)
