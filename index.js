82bb9ae97be0df52f7927c622e5e6f3d83c16d67
AlanGr12 <alanfutbol58@gmail.com>
2026-03-16 10:13:39 -0300

    tp 2 dai terminado

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
 
-
-
-//Ejercicio 4
-import fs from 'fs'
-
-function copiar(entrada, salida){
-const contenido  = fs.readFileSync(entrada,'utf-8')
-fs.writeFileSync(salida, contenido)
-}
-
-
-copiar('./entrada.txt', './salida.txt')
-console.log(`El archivo se creo perfectamente`)
